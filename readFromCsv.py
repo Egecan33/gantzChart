919b1961ec971378a90ab69808ceb2aa1f1dd06b
FrostyFox33 <43838681+FrostyFox33@users.noreply.github.com>
2023-04-08 00:01:54 +0300

    2.1.0
    Working csv reader as multiple job for machines and legend

--- readFromCsv.py
+++ readFromCsv.py
@@ -1,17 +1,22 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import csv
+from collections import defaultdict
 
 
 def create_gantt_chart(file_path):
-    # Read the job names and time intervals from the CSV file
-    jobs = []
-    time_intervals = []
+    # Read the machine names, job names, and time intervals from the CSV file
+    machines = []
+    jobs = defaultdict(list)
+    time_intervals = defaultdict(list)
     with open(file_path, "r") as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            jobs.append(row[0])
-            time_intervals.append((row[1], row[2]))
+            machine, job, start_time, end_time = row
+            if machine not in machines:
+                machines.append(machine)
+            jobs[machine].append(job)
+            time_intervals[machine].append((start_time, end_time))
 
     fig, ax = plt.subplots()
 
@@ -21,18 +26,41 @@ def create_gantt_chart(file_path):
 
     # Set the axis labels
     ax.set_xlabel("Date & Time")
-    ax.set_ylabel("Jobs")
+    ax.set_ylabel("Machines")
 
     # Compute the minimum and maximum time values from the input time intervals
-    start_times, end_times = zip(*time_intervals)
-    min_time = min(start_times)
-    max_time = max(end_times)
+    all_start_times = [
+        start_time
+        for machine_intervals in time_intervals.values()
+        for start_time, _ in machine_intervals
+    ]
+    all_end_times = [
+        end_time
+        for machine_intervals in time_intervals.values()
+        for _, end_time in machine_intervals
+    ]
+    min_time = min(all_start_times)
+    max_time = max(all_end_times)
 
-    for idx, job in enumerate(jobs):
-        start_time, end_time = time_intervals[idx]
-        start_num = mdates.datestr2num(start_time)
-        end_num = mdates.datestr2num(end_time)
-        ax.barh(job, end_num - start_num, left=start_num, height=0.3, align="center")
+    # Assign a color to each unique job
+    all_jobs = set(job for job_list in jobs.values() for job in job_list)
+    colors = plt.cm.get_cmap("tab20", len(all_jobs))
+    job_colors = {job: colors(i) for i, job in enumerate(all_jobs)}
+
+    # Plot the bars for each machine and job
+    for idx, machine in enumerate(machines):
+        for job, (start_time, end_time) in zip(jobs[machine], time_intervals[machine]):
+            start_num = mdates.datestr2num(start_time)
+            end_num = mdates.datestr2num(end_time)
+            ax.barh(
+                machine,
+                end_num - start_num,
+                left=start_num,
+                height=0.95,
+                align="center",
+                color=job_colors[job],
+                label=job,
+            )
 
     # Set the x-axis limits to the minimum and maximum time values, with a buffer of 5% on either side
     time_range = mdates.datestr2num(max_time) - mdates.datestr2num(min_time)
@@ -41,6 +69,14 @@ def create_gantt_chart(file_path):
         mdates.datestr2num(min_time) - buffer, mdates.datestr2num(max_time) + buffer
     )
 
+    # Set the x-axis label to include the minimum time value
+    ax.set_xlabel(f"Start Time: {min_time}   Date & Time")
+
+    # Create a legend for the job colors
+    handles, labels = ax.get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    ax.legend(by_label.values(), by_label.keys())
+
     # Set the layout and show the chart
     plt.tight_layout()
     plt.grid(axis="x")
@@ -48,6 +84,5 @@ def create_gantt_chart(file_path):
     plt.show()
 
 
-# Example usage:
 file_path = "jobs.csv"
 create_gantt_chart(file_path)
